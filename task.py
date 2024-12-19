3868dd3045d992d2e9277dbff5adcbabfa85518d
Dawid Szarwark <daq131@gmail.com>
2024-12-19 15:25:08 -0400

    change the name of the stores to a capital letter

--- task.py
+++ task.py
@@ -2,3 +2,7 @@ my_dict = {
     'piekarnia' : ["chleb", "pączek", "bułki"],
     'warzywniak': ["marchew", "seler", "rukola"]
 }
+
+for key, item in my_dict.items():
+    print(f'Idę do {key.capitalize()}, kupuję tu następujące rzeczy {item}')
+    
