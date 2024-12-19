1fa4724be6a6fd491cb6c68876bd334d9c636d1d
Dawid Szarwark <daq131@gmail.com>
2024-12-19 15:27:31 -0400

    add sum of products

--- task.py
+++ task.py
@@ -6,3 +6,6 @@ my_dict = {
 for key, item in my_dict.items():
     print(f'Idę do {key.capitalize()}, kupuję tu następujące rzeczy {item}')
     
+new = []
+new = my_dict['piekarnia'] + my_dict['warzywniak']
+print('W sumie kupuję', len(new), 'produktów')
