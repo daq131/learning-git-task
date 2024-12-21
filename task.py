c1202997e1d38dfc4e31c78296c918202490b8bd
Dawid Szarwark <daq131@gmail.com>
2024-12-21 11:42:48 -0400

    initial commit

--- task.py
+++ task.py
@@ -9,3 +9,7 @@ for key, item in my_dict.items():
 new = []
 new = my_dict['piekarnia'] + my_dict['warzywniak']
 print('W sumie kupuję', len(new), 'produktów')
+
+text = 'bla'
+print(3*'bla')
+print(1918-1795)
